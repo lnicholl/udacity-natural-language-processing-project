56992e3a208ef3167a388c894025625d973b1589
lnicholl <63803604+lnicholl@users.noreply.github.com>
2020-06-05 13:01:46 +0100

    Add files via upload

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,22 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value
+    document.getElementById('results').innerHTML = formText;
+    Client.checkForName(formText)
+
+    console.log("::: Form Submitted :::")
+    fetch('http://localhost:8081/aylienapi',
+    {
+        method: 'POST',
+        body: JSON.stringify({formText}),
+        headers: {"Content-Type": "application/json"}
+    })
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.message
+    }).catch(error => console.log(error));
+}
+
+export { handleSubmit }
---
+++ src/server/index.js
@@ -0,0 +1,58 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+var aylien = require('aylien_textapi')
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+
+// middleware
+const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+// post
+app.post('/aylienapi', async (req, res) => {
+    const inputText = req.body;
+    console.log(inputText);
+    
+    try {
+        textapi.sentiment({
+            'text': inputText 
+        }, function(error, response) {
+          if (error === null) {
+            console.log(response);
+            res.send(response);
+        }
+      });
+      
+    } catch(error) {
+      console.log(error)
+      return(error)
+    }
+  })
