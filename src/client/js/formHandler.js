49e5c1e3bcf5f5a5e6733c02d8864e9c9700341b
lnicholl <63803604+lnicholl@users.noreply.github.com>
2020-06-08 10:29:07 +0100

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -15,7 +15,7 @@ function handleSubmit(event) {
     })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = `Polarity: ${res.polarity}, Subjectivity: ${res.subjectivity}`   
     }).catch(error => console.log(error));
 }
 
