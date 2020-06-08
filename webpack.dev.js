37467c58d8503b09cc82ea0d20798384b18d295e
lnicholl <63803604+lnicholl@users.noreply.github.com>
2020-06-08 16:46:33 +0100

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,16 +2,23 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})],
+    },
     output: {
         libraryTarget: 'var',
         library: 'Client'
     },
-    devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
@@ -38,6 +45,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
